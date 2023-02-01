cdad9f4594db6d1448a537281a026ef0eba0a5c3
mohammedAbuzriba <mohamedabuzriba@hotmail.com>
2023-02-01 10:05:57 +0200

    add dualog

--- app/src/main/java/ly/youcan/mylistview/listview2/MyAdapter.kt
+++ app/src/main/java/ly/youcan/mylistview/listview2/MyAdapter.kt
@@ -1,31 +1,111 @@
 package ly.youcan.mylistview.listview2
 
-import android.annotation.SuppressLint
 import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Adapter
 import android.widget.ArrayAdapter
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.core.content.ContextCompat.startActivity
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import com.google.android.material.snackbar.Snackbar
+import ly.youcan.mylistview.MainActivity
 import ly.youcan.mylistview.R
+import ly.youcan.mylistview.UserActivity
 
-class MyAdapter(context:Context,names:ArrayList<Names>)
-    :ArrayAdapter<Names>(context, R.layout.costumviewnames,names){
+class CoustemAdapter(val context: Context,val names:ArrayList<Names>):RecyclerView.Adapter<CoustemAdapter.MyViewHolder>(){
 
-    @SuppressLint("ViewHolder")
-    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
-        val listviewitem = LayoutInflater.from(context).inflate( R.layout.costumviewnames,parent,false)
-        val name= getItem(position)
-
-
-        listviewitem.findViewById<TextView>(R.id.textView6).text = name!!.name
-        listviewitem.findViewById<TextView>(R.id.textView8).text = name!!.phon
-        listviewitem.findViewById<TextView>(R.id.textView7).text = name!!.age.toString()
-        listviewitem.findViewById<ImageView>(R.id.imageView4).setImageResource(name.image)
-
-        return listviewitem
+    class MyViewHolder(itemView: View): ViewHolder(itemView){
+        val txtView = itemView.findViewById(R.id.title) as TextView
+        val txtView2 = itemView.findViewById(R.id.body) as TextView
+        val imgView = itemView.findViewById(R.id.logo) as ImageView
+        val add = itemView.findViewById(R.id.add) as Button
+        val remov = itemView.findViewById(R.id.remov) as Button
     }
 
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        val listviewitem = LayoutInflater.from(parent.context).inflate( R.layout.listpranditem,parent,false)
+
+        return MyViewHolder(listviewitem)
+    }
+
+    override fun getItemCount(): Int {
+        return names.size
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        val name = names[position]
+        holder.txtView.text = name.name
+        holder.txtView2.text = name.phon
+        holder.imgView.setImageResource(name.image)
+
+        holder.imgView.setOnClickListener(){
+            Snackbar.make(it,name.name, Snackbar.LENGTH_SHORT).show()
+
+        }
+        holder.itemView.setOnClickListener(){
+            Snackbar.make(it,"UserId: ${name.name}\nId: ${name.age}", Snackbar.LENGTH_SHORT).show()
+
+            val i = Intent(context, UserActivity::class.java)
+            i.putExtra("name",name.name)
+            i.putExtra("phone",name.phon)
+            i.putExtra("country",name.age)
+            i.putExtra("imageId",name.image)
+
+            context.startActivity(i)
+
+        }
+
+        holder.add.setOnClickListener(){
+            addItem(position,name)
+        }
+        holder.remov.setOnClickListener(){
+            removeItem(position)
+        }
+
+    }
+
+    private fun addItem(position: Int, name: Names) {
+        names.add(position,name)
+        notifyItemRemoved(position)
+        notifyItemRangeChanged(position,names.size)
+
+    }
+
+
+    private fun removeItem(position: Int) {
+        names.removeAt(position)
+        notifyItemRemoved(position)
+        notifyItemRangeChanged(position,names.size)
+
+    }
+
+
+}
+
+
+
+
+fun myList(){
+    class MyAdapter(context:Context,names:ArrayList<Names>)
+        :ArrayAdapter<Names>(context, R.layout.costumviewnames,names){
+
+        override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+            val listviewitem = LayoutInflater.from(context).inflate( R.layout.costumviewnames,parent,false)
+            val name= getItem(position)
+
+
+            listviewitem.findViewById<TextView>(R.id.textView6).text = name!!.name
+            listviewitem.findViewById<TextView>(R.id.textView8).text = name!!.phon
+            listviewitem.findViewById<TextView>(R.id.textView7).text = name!!.age.toString()
+            listviewitem.findViewById<ImageView>(R.id.imageView4).setImageResource(name.image)
+
+            return listviewitem
+        }
+
+    }
 }
